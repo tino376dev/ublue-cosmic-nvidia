d1298899f68dfb39a4ee3ae3630d9535641d207d
forest reider <forest.reider@belimo.ch>
2025-06-14 13:13:05 +0200

    add swaybg and remove redundent xwayland satellites (already installed with niri)

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,30 +2,18 @@
 
 set -ouex pipefail
 
-### Install packages
-
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
-dnf5 install -y openfortivpn xwayland-satellite brightnessctl
-
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+# terminal emulator
 dnf -y copr enable pgdev/ghostty
 dnf -y install ghostty
 dnf5 -y copr disable pgdev/ghostty
 
+# niri
 dnf -y copr enable yalter/niri
 dnf -y install niri
 dnf5 -y copr disable yalter/niri
 
-#### Example for enabling a System Unit File
+# regular packages
+dnf5 install -y openfortivpn swaybg brightnessctl
 
+# podman socket
 systemctl enable podman.socket
