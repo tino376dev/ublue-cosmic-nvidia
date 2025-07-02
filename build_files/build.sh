b5bfeb405e6fa0a5abb06761e77edb8f6bac05c7
forest reider <forest.reider@belimo.ch>
2025-07-02 07:33:46 +0200

    Revert "try using drakulix extra-sessions - manual install"
    This reverts commit d90ef33e1f58d64b4a568f4da79c924e22da8d1c.

--- build_files/build.sh
+++ build_files/build.sh
@@ -17,7 +17,3 @@ dnf5 install -y openfortivpn mako swaybg brightnessctl nautilus
 
 # podman socket
 systemctl enable podman.socket
-
-## test using niri with rest of cosmic elements
-curl -o /sbin/start-cosmic-ext-niri https://raw.githubusercontent.com/drakulix/cosmic-ext-extra-sessions/main/niri/start-cosmic-ext-niri
-curl -o /usr/share/wayland-sessions/cosmic-ext-niri.desktop https://raw.githubusercontent.com/drakulix/cosmic-ext-extra-sessions/main/niri/cosmic-ext-niri.desktop
