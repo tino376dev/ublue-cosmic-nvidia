6cd7cd6b4ea206952f74c5666f616b0356fa084e
forest reider <forest.reider@belimo.ch>
2025-07-14 15:51:03 +0200

    niri without copr

--- build_files/build.sh
+++ build_files/build.sh
@@ -8,12 +8,12 @@ set -ouex pipefail
 # dnf5 -y copr disable pgdev/ghostty
 
 # niri
-dnf -y copr enable yalter/niri
-dnf -y install niri
-dnf5 -y copr disable yalter/niri
+# dnf -y copr enable yalter/niri
+# dnf -y install niri
+# dnf5 -y copr disable yalter/niri
 
 # regular packages
-dnf5 install -y openfortivpn brightnessctl nautilus
+dnf5 install -y niri openfortivpn brightnessctl nautilus
 
 # podman socket
 systemctl enable podman.socket
