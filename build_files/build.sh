e6c3483076baa10375e753eedebbf732de1230d7
forest reider <forest.reider@belimo.ch>
2025-07-16 07:51:32 +0200

    add +x permission to start-cosmic-ext-niri

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,22 +2,19 @@
 
 set -ouex pipefail
 
-# terminal emulator
-dnf -y copr enable pgdev/ghostty
-dnf -y install ghostty
-dnf5 -y copr disable pgdev/ghostty
-
-# niri
-dnf -y copr enable yalter/niri
-dnf -y install niri
-dnf5 -y copr disable yalter/niri
-
 # regular packages
-dnf5 install -y openfortivpn mako swaybg brightnessctl nautilus
+dnf5 install -y brightnessctl mako micro nautilus niri openfortivpn pavucontrol swaybg swayidle
 
 # podman socket
 systemctl enable podman.socket
 
 ## test using niri with rest of cosmic elements
-curl -o /sbin/start-cosmic-ext-niri https://raw.githubusercontent.com/drakulix/cosmic-ext-extra-sessions/main/niri/start-cosmic-ext-niri
+curl -o /usr/bin/start-cosmic-ext-niri https://raw.githubusercontent.com/drakulix/cosmic-ext-extra-sessions/main/niri/start-cosmic-ext-niri
 curl -o /usr/share/wayland-sessions/cosmic-ext-niri.desktop https://raw.githubusercontent.com/drakulix/cosmic-ext-extra-sessions/main/niri/cosmic-ext-niri.desktop
+chmod +x /usr/bin/start-cosmic-ext-niri
+
+# git clone https://github.com/Drakulix/cosmic-ext-extra-sessions.git
+# cd cosmic-ext-extra-sessions
+# git submodule update --init
+# just build
+# just install-niri
