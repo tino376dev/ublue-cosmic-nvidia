1984bf899a14839dd72fa4b9d872064afd72eb8c
forest reider <forest.reider@belimo.ch>
2025-07-16 08:26:46 +0200

    test drakulix extra-sessions - manual install

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,5 +5,11 @@ set -ouex pipefail
 # regular packages
 dnf5 install -y brightnessctl mako micro nautilus niri openfortivpn pavucontrol swaybg swayidle
 
+## test using niri with rest of cosmic elements
+curl -o /usr/bin/start-cosmic-ext-niri https://raw.githubusercontent.com/drakulix/cosmic-ext-extra-sessions/main/niri/start-cosmic-ext-niri
+curl -o /usr/share/wayland-sessions/cosmic-ext-niri.desktop https://raw.githubusercontent.com/drakulix/cosmic-ext-extra-sessions/main/niri/cosmic-ext-niri.desktop
+sudo chmod +x /usr/bin/start-cosmic-ext-niri
+##
+
 # podman socket
 systemctl enable podman.socket
