7718d9d54ea6df21fe367f0b1285c5fe6ac02933
forest reider <forest.reider@belimo.ch>
2025-08-29 10:48:11 +0200

    vscode-insiders has keyring issues with cosmic/niri

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,8 +2,8 @@
 
 set -ouex pipefail
 
-rpm --import https://packages.microsoft.com/keys/microsoft.asc
-sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+# rpm --import https://packages.microsoft.com/keys/microsoft.asc
+# sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
 # regular packages
 dnf install -y brightnessctl code-insiders gcc mako micro nautilus niri openfortivpn pavucontrol swaybg swayidle
